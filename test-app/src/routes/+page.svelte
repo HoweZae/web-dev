2616d93718969a07d152254c932cb14739cd4508
Jose <jdtomanan@up.edu.ph>
2024-07-27 20:11:06 +0800

    feat: almost done with 1 - Logic

--- test-app/src/routes/+page.svelte
+++ test-app/src/routes/+page.svelte
@@ -4,7 +4,7 @@
     import Lyric from './Lyric.svelte';
     import MultiLyric from './MultiLyric.svelte';
 
-	let title = 'svelte testing page';
+	let title = 'sveltekit';
     let svelteLogo = '/test.jpg';
     let song = "DBATC";
 
@@ -15,11 +15,18 @@
         count = [...count, count.length + 1]
     }
 
+    /* CONSTANTS */
     const multilyric1 = {
         line1: "Our country, guess it was a lawless land",
         line2: "Quiet my fears with the touch of your hands",
         line3: "My paper cut stings from my paper thin plans",
     }
+
+    const multilyric2 = [
+        {color: "text-blue-600", line: "Trying to find a part of me that you didn't take up"},
+        {color: "text-pink-600", line: "Gave you so much but it wasn't enough"},
+        {color: "text-purple-600", line: "But I'll be alright, it's just a thousand cuts"},
+    ]
 
     /* REACTIVES */
     $: countActual = count.length;
@@ -34,13 +41,15 @@
 <style>
     h1 { font-family: 'Charter' }
 	p {
-		color: violet;
+        /* color: salmon; */
 		font-family: 'Microsoft Sans Serif';
 		font-size: 1em;
 	}
 </style>
 
-<h1 class = "text-3xl font-bold underline">{ title.toUpperCase() }</h1>
+<h1 class="text-4xl font-bold italic my-2 center">
+    {title.toUpperCase()} Testing Page
+</h1>
 
 <img src={svelteLogo} alt="{song} Lyrics" />
 
@@ -48,9 +57,25 @@
 <Lyric lyric="Trying to find a part of me that you didn't touch"/>
 <p>{@html htmlLine}</p>
 
-<button class="bg-pink-500 hover:bg-pink-600 active:bg-pink-700 focus:outline-none focus:ring focus:ring-pink-300" on:click = {increment}>
+<button
+    class="bg-pink-500 hover:bg-pink-600 active:bg-pink-700 focus:outline-none focus:ring focus:ring-pink-300 ml-2 my-2 px-2 py-1 rounded-xl font-semibold"
+    on:click = {increment}
+><span class="text-pink-100">
     Now I'm searching for {countActual} {countActual === 1 ? 'sign' : 'signs'} in {doubled} haunted clubs
+    </span>
 </button>
 
 <Lyric />
 <MultiLyric {...multilyric1}/>
+
+{#if count <= 2}
+    <p>My time, my wine, my spirit, my trust</p>
+    {:else}
+        <p>My times, my wines, my spirits, my trusts</p>
+{/if}
+
+<div class="bg-slate-100 p-2 m-2">
+    {#each multilyric2 as item}
+        <p class={item.color}>{item.line}</p>
+    {/each}
+</div>
--- test-app/src/routes/Lyric.svelte
+++ test-app/src/routes/Lyric.svelte
@@ -2,4 +2,10 @@
     export let lyric = "Our songs, our films, united we stand";
 </script>
 
+<style>
+    p {
+        color: salmon;
+    }
+</style>
+
 <p>{lyric}</p>
